4ac1fab5c1e64961f758d19dca9a9bd9e929ba9b
hunit <hunit612@naver.com>
2024-03-20 23:52:42 +0900

    초기 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+Error.stackTraceLimit = Infinity;
+
+const envFilePath = process.env.NODE_ENV === 'production' ? '.env' : '.env.dev';
+const logger = new Logger('bootstrap');
+logger.log(`Application is running in ${process.env.NODE_ENV} mode`);
+
+dotenv.config({ path: envFilePath });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

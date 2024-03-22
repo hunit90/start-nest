bbf30b69a5eeba4e821b01a4f5ab371ff1333000
hunit <hunit612@naver.com>
2024-03-22 23:19:48 +0900

    swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { BaseAPIDocument } from './config/swagger.documents';
+import { SwaggerModule } from '@nestjs/swagger';
 
 Error.stackTraceLimit = Infinity;
 
@@ -13,6 +15,10 @@ dotenv.config({ path: envFilePath });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
